de65f201bf54e249290b877394107e77cda9d697
yukiharada1228 <yukiharada1228@gmail.com>
2023-01-28 03:22:00 +0900

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-UPLOAD_FOLDER = 'static/uploads/'
+UPLOAD_FOLDER = "static/uploads/"
 
 app = Flask(__name__)
 app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
--- main.py
+++ main.py
@@ -1,31 +1,70 @@
+import logging
 import os
-from app import app
-from flask import flash, request, redirect, url_for, render_template
+import sys
+
+import cv2 as cv
+from flask import flash, redirect, render_template, request, url_for
 from werkzeug.utils import secure_filename
-	
-@app.route('/')
+
+from app import app
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+import models.smile_detection.face_detection.face_detection.config as face_detection
+import models.smile_detection.smile_detection.config as smile_detection
+
+filename = ""
+face_detect = face_detection.face_detect
+smile_recognition = smile_detection.smile_recognition
+
+
+@app.route("/")
 def upload_form():
-	return render_template('upload.html')
+    return render_template("upload.html")
 
-@app.route('/', methods=['POST'])
+
+@app.route("/upload", methods=["POST"])
 def upload_video():
-	if 'file' not in request.files:
-		flash('動画がありませんでした．')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('アップロードする動画が選択されていませんでした．')
-		return redirect(request.url)
-	else:
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		flash('動画がアップロードされました．')
-		return render_template('upload.html', filename=filename)
+    global filename
+    if "file" not in request.files:
+        flash("動画がありませんでした．")
+        return redirect(request.url)
+    file = request.files["file"]
+    if file.filename == "":
+        flash("アップロードする動画が選択されていませんでした．")
+        return redirect(request.url)
+    else:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        flash(f"{filename}がアップロードされました．")
+        return render_template("upload.html", filename=filename)
 
-@app.route('/display/<filename>')
+
+@app.route("/execute", methods=["POST"])
+def execute():
+    global filename
+    video_frame_count = 0
+    capture = cv.VideoCapture("static/uploads/" + filename)
+    probs_max = 0.0
+    while capture.isOpened():
+        ret, frame = capture.read()
+        if not ret:
+            break
+        video_frame_count += 1
+        faces = face_detect.detect(frame)
+        probs = smile_recognition.recognize(frame, faces)
+        if len(probs):
+            if max(probs) > probs_max:
+                probs_max = max(probs)
+                logger.info({"action": "execute", "probs_max": probs_max})
+                cv.imwrite("static/imgs/output.jpg", frame)
+    return render_template("index.html")
+
+
+@app.route("/display/<filename>")
 def display_video(filename):
-	return redirect(url_for('static', filename='uploads/' + filename), code=301)
+    return redirect(url_for("static", filename="uploads/" + filename), code=301)
+
 
 if __name__ == "__main__":
     app.run()
-	
