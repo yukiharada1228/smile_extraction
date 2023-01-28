fb75e411de91653ffcccd1a7306ec1e44aec4ec1
yukiharada1228 <yukiharada1228@gmail.com>
2023-01-28 14:52:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
+from pathlib import Path
+
 from flask import Flask
 
-UPLOAD_FOLDER = "static/uploads/"
+PROJECT_ROOT = Path(__file__).resolve().parent
+STATIC_FOLDER = PROJECT_ROOT / "static"
+UPLOAD_FOLDER = STATIC_FOLDER / "uploads"
+IMAGE_FOLDER = STATIC_FOLDER / "imgs"
+
+if not STATIC_FOLDER.exists():
+    for folder in [STATIC_FOLDER, UPLOAD_FOLDER, IMAGE_FOLDER]:
+        folder.mkdir()
 
 app = Flask(__name__)
 app.secret_key = "secret key"
